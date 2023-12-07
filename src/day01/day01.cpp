e3643b94524c1c874e349ba83c234382a5e1ebbe
eigemx <70483607+eigemx@users.noreply.github.com>
2023-12-07 06:32:37 +0200

    Renove unused code in day01.cpp

--- src/day01/day01.cpp
+++ src/day01/day01.cpp
@@ -10,20 +10,7 @@
 #include <string_view>
 #include <utility>
 
-// clang-format off
-// prepare string representation of digits
-std::vector<std::string> str_numbers = {
-    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    "1",   "2",   "3",     "4",    "5",    "6",   "7",     "8",     "9"};
-
-std::map<std::string, int> num_str_to_int{
-    {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
-    {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"1", 1},
-    {"2", 2},   {"3", 3},     {"4", 4},     {"5", 5},    {"6", 6},
-    {"7", 7},   {"8", 8},     {"9", 9}};
-// clang-format on
-
-// part I sum function
+// part I
 [[nodiscard]] inline auto sum_numbers(std::string_view text) -> unsigned int;
 
 // part II
